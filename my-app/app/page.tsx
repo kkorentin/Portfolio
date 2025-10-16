3ac5d250f4b51c448d8ab1ea71e5415aa9e03d24
kkorentin <korentin.kolaczek@gmail.com>
2025-10-16 12:25:27 +0200

    feat/add profil picture and modif navbar icons

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
-  return (
-      <div className="grid place-items-center">
-        <p className="text-2xl lg:text-3xl">Bienvenue sur mon portfolio</p>
-      </div>
-  );
+    return (
+        <div className="max-w-screen-xl mx-auto px-4">
+            <div className="flex flex-row items-center justify-center gap-8 mt-16">
+                <div id="descriptionProfil" className="w-60 text-right">
+                    <h1 className="font-bold">Développeur Fullstack</h1>
+                </div>
+                <img
+                    src="/profil.jpg"
+                    alt="ProfilPic"
+                    className="h-80 w-60 md:h-110 md:w-70 object-cover rounded-full shadow-lg mx-4"
+                />
+                <div className="w-60 text-left">
+                    <p className="text-sm font-semibold text-gray-700">Texte droite</p>
+                </div>
+            </div>
+        </div>
+    );
 }
--- my-app/components/common/navbar.tsx
+++ my-app/components/common/navbar.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import React from "react";
 
 export default function Navbar() {
@@ -8,21 +6,22 @@ export default function Navbar() {
         <nav className="w-full bg-white ">
             <div className="relative flex items-center max-w-screen-xl h-16 mx-auto px-4">
                 <div className="flex-1 flex items-center gap-8">
-                    <Link href="/" className="uppercase tracking-wide text-sm font-semibold hover:text-gray-700">A propos</Link>
-                    <Link href="/project" className="uppercase tracking-wide text-sm font-semibold hover:text-gray-700">Projets</Link>
-                    <Link href="/contact" className="uppercase tracking-wide text-sm font-semibold hover:text-gray-700">Contact</Link>
-                </div>
-                <div className="absolute left-1/2 transform -translate-x-1/2">
-                    <p className="uppercase tracking-wide text-sm font-semibold">Korentin</p>
+                    <Link href="/" className="uppercase tracking-wide text-sm font-semibold hover:text-gray-700">A
+                        propos</Link>
+                    <Link href="/project"
+                          className="uppercase tracking-wide text-sm font-semibold hover:text-gray-700">Projets</Link>
+                    <Link href="/contact"
+                          className="uppercase tracking-wide text-sm font-semibold hover:text-gray-700">Contact</Link>
                 </div>
                 <div className="flex-1 flex justify-end items-center gap-4">
                     <div className="flex justify-end items-center gap-3 w-24">
                         <a href="https://github.com/kkorentin" aria-label="GitHub" target="_blank"
                            rel="noopener noreferrer">
-                            <GitHubIcon className="text-black hover:text-gray-700"/>
+                            <img src="/icons8-github.svg" alt="GitHub" className="h-6 w-6 hover:opacity-70"/>
                         </a>
-                        <a href="#" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer">
-                            <LinkedInIcon className="text-black hover:text-gray-700"/>
+                        <a href="www.linkedin.com/in/korentin-kolaczek" aria-label="LinkedIn" target="_blank"
+                           rel="noopener noreferrer">
+                            <img src="/icons8-linkedin.svg" alt="LinkedIn" className="h-6 w-6 hover:opacity-70"/>
                         </a>
                     </div>
                 </div>
