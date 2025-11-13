c0449613ea496b37345b9b704f61c5177af8edbe
kkorentin <korentin.kolaczek@gmail.com>
2025-11-13 13:50:45 +0100

    fix/experience card input

--- my-app/components/common/experienceCard.tsx
+++ my-app/components/common/experienceCard.tsx
@@ -5,7 +5,7 @@ import SkillsDecoration from "@/components/common/skillsDecoration";
 
 
 export default function ExperienceCard({ date, title, description, skills }: ExperienceCardProps) {
-    const skillsArray = "Arduino,PhP,HTML,CSS".split(',').map(s => s.trim());
+    const skillsArray = skills.split(',').map(s => s.trim());
     const descriptionArray = description.split('•').map(s => s.trim()).filter(s => s);
     return (
         <div className="relative bg-amber-50 max-w-[50%] p-4">
@@ -14,7 +14,7 @@ export default function ExperienceCard({ date, title, description, skills }: Exp
                 <div className="pl-8">
                     <p className="text-sm text-gray-700" id="titleExperience">{title}</p>
                     <div className="text-sm font-semibold text-gray-700 mt-2" id="description">{descriptionArray.map((desc,idx)=>(
-                        <p id={idx.toString()}>• {desc}</p>
+                        <p key={idx} id={idx.toString()}>• {desc}</p>
                     ))}</div>
                     <div className="text-sm text-gray-700 flex flex-row pt-2 space-x-1" id="skillExperience">{skillsArray.map((skill,idx)=>(
                         <SkillsDecoration skillsDeco={skill} key={idx} />
